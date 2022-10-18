efd42ff1a2d33f17b71f739567e2600018adcd8f
kajikentaro <kajindowsxp@gmail.com>
2022-10-19 02:00:11 +0900

    Fix: mysqlのエラー修正、indexを追加

--- db/docker-entrypoint-initdb.sql
+++ db/docker-entrypoint-initdb.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE grpc;
-CREATE TABLE user(id text PRIMARY KEY, name TEXT, email TEXT);
-CREATE TABLE audio(id int AUTO_INCREMENT PRIMARY KEY, user_id text KEY, audioName text, description text, url text);
-CREATE TABLE tag(id int AUTO_INCREMENT PRIMARY KEY, user_id text KEY, audio_id int KEY, int64 start_ms, int64 end_ms, tagName text);
-
+use grpc;
+CREATE TABLE user(id char(30) PRIMARY KEY, name TEXT, email TEXT);
+CREATE TABLE audio(id int AUTO_INCREMENT PRIMARY KEY, user_id char(30), audio_name text, description text, url text);
+CREATE TABLE tag(id int AUTO_INCREMENT PRIMARY KEY, user_id char(30), audio_id int, start_ms int, end_ms int, tag_name text);
+CREATE INDEX audio_idx on audio(user_id);
+CREATE INDEX tag_idx on tag(user_id, audio_id);
