c285b5472eb0d5030fb69ad8ae26606c598a76fd
kajikentaro <kajindowsxp@gmail.com>
2022-10-22 06:48:42 +0900

    Add: apiからAudioを取得できるように

--- frontend/src/pages/menu/playing/[id].tsx
+++ frontend/src/pages/menu/playing/[id].tsx
@@ -1,44 +1,63 @@
-import Styles from "../../../styles/playing.module.scss";
-import { useState } from "react";
-import MovieAudioPlayer from "../../components/MovieAudioPlayer";
-import { AudioInfo, SectionInfo } from "../../components/interface";
 import { Fab } from "@mui/material";
+import { RpcError } from "grpc-web";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { TopPageClientClient } from "../../../../grpc_out/GrpcServiceClientPb";
+import { Audio, AudioList, Empty } from "../../../../grpc_out/grpc_pb";
+import MovieAudioPlayer from "../../../components/MovieAudioPlayer";
+import Styles from "../../../styles/playing.module.scss";
+import { BACKEND_ORIGIN } from "../../sample_api";
 
 const Playing = () => {
   // データを取ってくる．
+  const router = useRouter();
+  const {audioId} = router.query
+  const [my_audio_infos, my_audio_infos_set] = useState<Audio>(new Audio())
+
+  // 初回のみ実行
+   useEffect(()=>{
+    const client = new TopPageClientClient(BACKEND_ORIGIN + "", null, {
+      withCredentials: true,
+    });
+    const query = new Empty();
+    client.fetchAudioList(query, null, (err: RpcError, response: AudioList)=>{
+      if(err){
+        console.error(err.message);
+      }else{
+        if(typeof audioId !== "string"){
+        console.error(err.message);
+        return
+        }
+        const audioList = response.getAudiosList()
+        const audio = audioList.find(v=>{v.getId() + "" ===  audioId});
+        my_audio_infos_set(audio)
+      }
+    });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
+
   
   const [indList, indListSet] = useState<number>(0);
-  const my_audio_list: SectionInfo[] = [
-    { name: "zzz", start: 1, end: 3 },
-    { name: "aaa", start: 0, end: 5 },
-  ];
 
-  const my_audio_infos = new AudioInfo(
-    "タイトル1",
-    "/s.mp4", // 音声を動画にする．
-    "#b2f1a3",
-    "テストです....",
-    my_audio_list
-  );
 
   if (typeof window === "object") {
     document.oncontextmenu = function () {
       return false;
     };
   }
-  console.log(indList)
+  const playingTag = my_audio_infos.getTagsList()[indList]
 
   return (
     <div className={Styles.papars_wrap}>
       <div className={Styles.papar_main}>
         <div className={Styles.papar_line}>
           <div className={Styles.wrap}>
-            <div className={Styles.title}>{my_audio_infos.title}</div>
-            <div className={Styles.title}>{my_audio_list[indList].name}</div>
+            <div className={Styles.title}>{my_audio_infos.getAudioname()}</div>
+            <div className={Styles.title}>{playingTag.getTagname()}</div>
             <MovieAudioPlayer
-              source={my_audio_infos.audioroute}
-              startMs={my_audio_list[indList].start * 1000}
-              endMs={my_audio_list[indList].end * 1000}
+              source={my_audio_infos.getUrl()}
+              startMs={playingTag.getStartms() * 1000}
+              endMs={playingTag.getEndms() * 1000}
               tagNumber={indList}
             />
             <Fab
@@ -56,7 +75,7 @@ const Playing = () => {
               variant="extended"
               onClick={() => {
                 const tag_num = indList + 1;
-                if (tag_num < my_audio_list.length) {
+                if (tag_num < my_audio_infos.getTagsList().length) {
                   indListSet(tag_num);
                 }
               }}
